e0e43e1fbf5c5d975c110fcb6095861561725eb5
EL-Statslab <benoit.epaillard@gmail.com>
2026-04-15 12:37:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,16 +18,6 @@ import streamlit as st
 from matplotlib.gridspec import GridSpec
 from PIL import Image
 
-st.html("""
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-0RCT1FNM8V"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-0RCT1FNM8V');
-</script>
-""")
-
 # =============================================================================
 # CONFIG
 # =============================================================================
